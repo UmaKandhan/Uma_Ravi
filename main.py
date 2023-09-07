559fa7884504964ce096299ce5f3248230c7deb3
UmaKandhan <144319204+UmaKandhan@users.noreply.github.com>
2023-09-07 12:51:27 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,10 @@
+'''Facorial program
+challange'''
+def factorial(n):
+     
+    # single line to find factorial
+    return 1 if (n==1 or n==0) else n * factorial(n - 1)
+ 
+# Driver Code
+num =int(input("enter the value of n"))
+print("Factorial of",num,"is",factorial(num))
